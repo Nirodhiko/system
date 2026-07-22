fce4003ae1212fb38304540e75e9805b1d8ad920
Niro <nirodhiko@outlook.com>
2026-07-22 16:25:27 +0500

    remove all unnecessary files.

--- deploy.sh
+++ deploy.sh
@@ -32,7 +32,7 @@ link() {
 }
 
 # ~/.config/<name>
-for entry in alacritty espanso fish niri sioyek swayimg swaylock swaync waybar wallpapers yazi; do
+for entry in alacritty espanso fish sioyek swayimg wallpapers yazi; do
   link "$SYSTEM_DIR/$entry" "$HOME/.config/$entry"
 done
 
